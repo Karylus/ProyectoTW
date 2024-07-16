44e3a48610fd69bad7bfbf866bd8094c64d6fe86
Juan Pedro Moreno <57725792+Karylus@users.noreply.github.com>
2024-07-16 21:21:20 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-const DBHOST = 'localhost';
-const DBPORT = '3306';
-const DBUSER = 'jpedrojantonio2324';
-const DBPASSWORD = 'KKlESKIV6wj2X9SV';
-const DBDATABASE = 'jpedrojantonio2324';
+const DBHOST = '#';
+const DBPORT = '#';
+const DBUSER = '#';
+const DBPASSWORD = '#';
+const DBDATABASE = '#';
 ?>
